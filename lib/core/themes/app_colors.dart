136ffbcbdeb6d200c80174141c0f67b4d722c6fb
MohamedRefay <Mrefayy2000@gmail.com>
2024-04-08 03:35:57 +0200

    - update vars names to be more logicable

--- lib/core/themes/app_colors.dart
+++ lib/core/themes/app_colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color kPurple = Color(0xffA64EDD);
-  static const Color kBlue = Color(0xff6090DE);
-  static const Color kCian = Color(0xff17D4E0);
+  static const Color purple = Color(0xffA64EDD);
+  static const Color blue = Color(0xff6090DE);
+  static const Color cian = Color(0xff17D4E0);
   static const Color deepGrey = Color(0xff3c3c3c);
   static const Color black = Color(0xff181818);
   static const Color white = Color(0xffffffff);
