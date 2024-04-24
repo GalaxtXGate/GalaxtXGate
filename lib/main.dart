625b988738ea06dae4c286090df0039171109d6a
DESKTOP-D0I1DC1\test <asmaasedeek97@gmail.com>
2024-04-24 23:40:28 +0200

    add translations at assets folder and add easy localization plugin in pubspect.yamel file and wrap my app with EasyLocalization

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:galaxyxgate/core/helpers/cache_helper.dart';
 import 'package:galaxyxgate/core/routes/router.dart';
 import 'package:galaxyxgate/galaxy_x_gate_app.dart';
+import 'package:easy_localization/easy_localization.dart';
 
+import 'core/utils/app_assets.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   CacheHelper.init();
-  runApp(const MyApp());
+  runApp(
+      EasyLocalization(
+        path: AppAssets.appTranslations,
+        supportedLocales: const [
+          Locale('en'),
+          Locale('ar'),
+        ],
+        fallbackLocale: const Locale('en'),
+        startLocale: const Locale('ar'),
+        child: const MyApp(),
+      )
+  );
 }
 
 class MyApp extends StatelessWidget {
