f916d9f0594da03176f346d2866ceb0eade724f8
MohamedRefay <Mrefayy2000@gmail.com>
2024-04-28 22:15:25 +0300

    added authntication - login - signup - store profilePic - get Profile Pic - Store profile pic in database

--- lib/galaxy_x_gate_app.dart
+++ lib/galaxy_x_gate_app.dart
@@ -17,7 +17,10 @@ class GalaxyXGateApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(375, 812),
+      designSize: const Size(
+        375,
+        812,
+      ),
       minTextAdapt: true,
       child: MaterialApp(
         title: 'GalaxyXGate',
